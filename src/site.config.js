1c18909c26dbb00d70eaf3c08b45bfce03dff66f
Jack Reichert <awesome@jackreichert.com>
2021-04-30 18:40:24 -0400

    brings back site config

---
+++ src/site.config.js
@@ -0,0 +1,8 @@
+export default {
+    wpSite: 'jackreichert.wordpress.com',
+    siteAuthor: 'Jack Reichert',
+    perPage: 10,
+    traverseID: 1209197,
+    gtag: "UA-41027480-1",
+    twitter: "@jackreichert"
+}
