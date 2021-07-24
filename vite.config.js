819ebf003a57e567100c3ded65f79a84c6da1534
Jack Reichert <awesome@jackreichert.com>
2021-07-24 15:30:01 -0400

    updated vite aliases

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,11 @@
 // vite.config.js
 import vue from '@vitejs/plugin-vue'
-import {getAliases} from 'vite-aliases'
-
-getAliases({
-    // Path to the project Directory
-    path: './src/',
-
-    // Prefix Symbol for the Aliases
-    prefix: '@',
-
-    // Allow global project Directory alias
-    allowGlobalAlias: true
-});
-
+import {ViteAliases} from 'vite-aliases'
 
 /**
  * https://vitejs.dev/config/
  * @type {import('vite').UserConfig}
  */
 export default {
-    plugins: [vue()]
+    plugins: [vue(), ViteAliases()]
 }
